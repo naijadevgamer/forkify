09108a597fa166bb59632218897be30b4c11b5f2
thenaijagamer <sabdullahialaba50@gmail.com>
2024-04-04 13:09:45 +0100

    Reduce the result per view for mobile search result

--- src/js/config.js
+++ src/js/config.js
@@ -1,6 +1,6 @@
 export const API_URL = 'https://forkify-api.herokuapp.com/api/v2/recipes/';
 export const TIMEOUT_SEC = 10;
 export const RES_PER_PAGE = 10;
-export const RES_PER_PAGE_MOBILE = 7;
+export const RES_PER_PAGE_MOBILE = 5;
 export const MODAL_CLOSE_SEC = 2.5;
 export const API_KEY = '763e47ed-f380-4bd6-b269-09971a80bf7e';
